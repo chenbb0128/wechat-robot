93b86469d07c3e7b687208e78ef3a0315a8a0543
chen.hua <chen.hua@danatech.com>
2021-09-03 13:08:21 +0800

    移除自动添加好友

--- src/listeners/onFriendShip.js
+++ src/listeners/onFriendShip.js
@@ -9,22 +9,25 @@ module.exports = async function onFriendShip(friendship) {
   try {
     const friend = friendship.contact()
     logMsg = "添加好友" + friend.name()
-    console.log(logMsg);
     switch (friendship.type()) {
       case Friendship.Type.Receive:
-        const addFriendGapTime = randomNum(6, 10) * 10000
-        await delay(addFriendGapTime)
-        logMsg = `自动通过验证，因为验证消息是"${friendship.hello()}"`
-        await friendship.accept()
-        await user.addFriend(friend)
-        // 如果要发送MSG，有时需要延迟
-        const replyFriendGapTime = randomNum(1, 5) * 1000
-        await delay(replyFriendGapTime)
-        // 发送消息给添加的人
-        await friendship.contact().say('谢谢你加我，你笑起来真好看～')
+        // 添加新的好友，暂时开放自动添加好友功能
+        console.log(logMsg)
+        // const addFriendGapTime = randomNum(6, 10) * 10000
+        // await delay(addFriendGapTime)
+        // logMsg = `自动通过验证，因为验证消息是"${friendship.hello()}"`
+        // await friendship.accept()
+        // await user.addFriend(friend)
+        // // 如果要发送MSG，有时需要延迟
+        // const replyFriendGapTime = randomNum(1, 5) * 1000
+        // await delay(replyFriendGapTime)
+        // // 发送消息给添加的人
+        // await friendship.contact().say('谢谢你加我，你笑起来真好看～')
         break;
       case Friendship.Type.Confirm:
-        logMsg = "friend ship confirmed with " + friendship.contact().name();
+        await user.addFriend(friend)
+        await friendship.contact().say('谢谢你加我，你笑起来真好看～')
+        console.log('添加好友成功,'+ friendship.contact().name())
         break;
     }
     console.log(logMsg);
