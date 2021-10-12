4bf151e3b6083c6720d0c06b4c7d9889ff1bc8c7
chen.hua <chen.hua@danatech.com>
2021-10-13 00:41:49 +0800

    心跳测试

--- src/schecdule/heartbeat.js
+++ src/schecdule/heartbeat.js
@@ -1,8 +1,10 @@
 const schedule = require('./index');
+const schedule2 = require('node-schedule');
 
 async function heartbeat(bot) {
-  let rule = new schedule.RecurrenceRule()
-  rule.minute = [0, 5, 45]
+  let rule = new schedule2.RecurrenceRule()
+  rule.minute = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
+  console.log('心跳测试')
   schedule.setSchedule(rule, async () => {
     console.log('5分钟执行一次')
     console.log(new Date())
